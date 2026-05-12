8ad4adcb11317562b7d80fc6df088675eac3e624
Abdelrahman Sakr <abdelrahman.mansour04@eng-st.cu.edu.eg>
2026-05-12 23:45:53 +0300

    Attempting route53_record

--- modules/route53/main.tf
+++ modules/route53/main.tf
@@ -14,11 +14,7 @@ resource "aws_route53_zone" "main" {
 resource "aws_route53_record" "api" {
   zone_id = aws_route53_zone.main.zone_id
   name    = "api.${var.domain_name}"
-  type    = "A"
-
-  alias {
-    name                   = var.alb_dns_name
-    zone_id                = var.alb_zone_id
-    evaluate_target_health = true
-  }
+  type    = "CNAME"
+  ttl     = 300
+  records = [var.alb_dns_name]
 }
